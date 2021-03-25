6cd32cce2ff835e5eed9dd77131939cd0de694fe
matsjs <matsjorgen@hotmail.com>
2021-03-25 17:03:11 +0100

    chore: bump nox and nox poetry

--- noxfile.py
+++ noxfile.py
@@ -9,13 +9,12 @@ locations = "src", "tests", "noxfile.py"
 nox.options.sessions = ("lint", "mypy", "safety", "integration_tests")
 
 
-@nox.session
+@nox_poetry.session
 def integration_tests(session: Session) -> None:
     """Run the integration test suite."""
     args = session.posargs or ["--cov"]
-    nox_poetry.installroot(session, distribution_format=nox_poetry.WHEEL)
-    nox_poetry.install(
-        session,
+    session.install(
+        ".",
         "coverage[toml]",
         "pytest",
         "pytest-cov",
@@ -34,20 +33,19 @@ def integration_tests(session: Session) -> None:
     )
 
 
-@nox.session
+@nox_poetry.session
 def black(session: Session) -> None:
     """Run black code formatter."""
     args = session.posargs or locations
-    nox_poetry.install(session, "black")
+    session.install("black")
     session.run("black", *args)
 
 
-@nox.session
+@nox_poetry.session
 def lint(session: Session) -> None:
     """Lint using flake8."""
     args = session.posargs or locations
-    nox_poetry.install(
-        session,
+    session.install(
         "flake8",
         "flake8-annotations",
         "flake8-bandit",
@@ -60,7 +58,7 @@ def lint(session: Session) -> None:
     session.run("flake8", *args)
 
 
-@nox.session
+@nox_poetry.session
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     with tempfile.NamedTemporaryFile() as requirements:
@@ -73,21 +71,21 @@ def safety(session: Session) -> None:
             f"--output={requirements.name}",
             external=True,
         )
-        nox_poetry.install(session, "safety")
+        session.install("safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
-@nox.session
+@nox_poetry.session
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or locations
-    nox_poetry.install(session, "mypy")
+    session.install("mypy")
     session.run("mypy", *args)
 
 
-@nox.session
+@nox_poetry.session
 def coverage(session: Session) -> None:
     """Upload coverage data."""
-    nox_poetry.install(session, "coverage[toml]", "codecov")
+    session.install("coverage[toml]", "codecov")
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
