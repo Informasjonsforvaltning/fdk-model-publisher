235a8554ea41d0f5dd7969bcda99cb62fe4fd5f4
Stig B. Dørmænen <stigbd@gmail.com>
2021-11-19 10:45:21 +0100

    fix: change to standard response.json()

--- tests/mocks/__init__.py
+++ tests/mocks/__init__.py
@@ -1,5 +1,4 @@
 """Initialize mocks."""
 
 from .data_services_catalog_ttl import data_services_catalog_ttl_mock
-from .skagerrak_sparebank_json import skagerrak_sparebank_json_mock
 from .skagerrak_sparebank_ttl import skagerrak_sparebank_ttl_mock
